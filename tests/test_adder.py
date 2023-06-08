ae7af401c6ee65d9ff58cc65b643f7b3f116d098
Senthil Murugan Gnanasekaran <senthilmurukang@gmail.com>
2023-06-08 19:23:05 -0400

    added tests for adder

--- tests/test_adder.py
+++ tests/test_adder.py
@@ -1,38 +1,78 @@
+import os
+import random
+import sys
+from pathlib import Path
+
 import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge
-from cocotb.result import TestFailure
-from cocotb.regression import TestFactory
+from cocotb.runner import get_runner
+from cocotb.triggers import Timer
 
-@cocotb.coroutine
-def reset_dut(dut, duration):
-    dut.reset_n <= 0
-    yield cocotb.triggers.Timer(duration)
-    dut.reset_n <= 1
-    yield cocotb.triggers.Timer(duration)
+if cocotb.simulator.is_running():
+    from adder_model import adder_model
 
-@cocotb.coroutine
-def adder_test(dut):
-    A = 0b1010
-    B = 0b0110
-    expected_sum = 0b10000
-
-    dut.A <= A
-    dut.B <= B
-
-    yield RisingEdge(dut.clk)
-
-    if dut.sum != expected_sum:
-        raise TestFailure("Incorrect sum. Expected {}, got {}".format(bin(expected_sum), bin(dut.sum)))
 
 @cocotb.test()
-def adder_testbench(dut):
-    clock = Clock(dut.clk, 10, units="ns")
-    cocotb.fork(clock.start())
+async def adder_basic_test(dut):
+    """Test for 5 + 10"""
 
-    yield reset_dut(dut, 20)
-    yield adder_test(dut)
+    A = 5
+    B = 10
 
-# Running the testbench
-factory = TestFactory(test_function=adder_testbench)
-factory.generate_tests()
+    dut.A.value = A
+    dut.B.value = B
+
+    await Timer(2, units="ns")
+
+    assert dut.sum.value == adder_model(
+        A, B
+    ), f"Adder result is incorrect: {dut.sum.value} != 15"
+
+
+@cocotb.test()
+async def adder_randomised_test(dut):
+    """Test for adding 2 random numbers multiple times"""
+
+    for i in range(10):
+
+        A = random.randint(0, 7)
+        B = random.randint(0, 7)
+
+        dut.A.value = A
+        dut.B.value = B
+
+        await Timer(2, units="ns")
+
+        assert dut.sum.value == adder_model(
+            A, B
+        ), "Randomised test failed with: {A} + {B} = {sum}".format(
+            A=dut.A.value, B=dut.B.value, sum=dut.sum.value
+        )
+
+
+def test_adder_runner():
+    """Simulate the adder example using the Python runner.
+
+    This file can be run directly or via pytest discovery.
+    """
+    sim = os.getenv("SIM", "icarus")
+
+    proj_path = Path(__file__).resolve().parent.parent
+    # equivalent to setting the PYTHONPATH environment variable
+    sys.path.append(str(proj_path / "model"))
+
+    verilog_sources = [proj_path / "hdl" / "adder.sv"]
+
+    # equivalent to setting the PYTHONPATH environment variable
+    sys.path.append(str(proj_path / "tests"))
+
+    runner = get_runner(sim)
+    runner.build(
+        verilog_sources=verilog_sources,
+        hdl_toplevel="adder",
+        always=True,
+    )
+    runner.test(hdl_toplevel="adder", test_module="test_adder")
+
+
+if __name__ == "__main__":
+    test_adder_runner()
